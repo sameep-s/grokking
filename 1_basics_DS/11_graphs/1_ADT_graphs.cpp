c93d849645369ce1a7b9ca4f02094af362b58345
sameep <sameep.sharma98@gmail.com>
2024-09-01 03:12:00 +0530

    ADT: Graph

--- 1_basics_DS/11_graphs/1_ADT_graphs.cpp
+++ 1_basics_DS/11_graphs/1_ADT_graphs.cpp
@@ -73,6 +73,75 @@ void Graph::add_edge(int vertex1, int vertex2)
     adjacencyList[vertex2].push_back(vertex1);
 }
 
+// Remove edge
+void Graph::remove_edge(int vertex1, int vertex2)
+{
+    // Remove the edge
+    auto neighbours1 = adjacencyList[vertex1];
+    auto neighbours2 = adjacencyList[vertex2];
+
+    neighbours1.erase(remove(neighbours1.begin(), neighbours1.end(), vertex2), neighbours1.end());
+    neighbours2.erase(remove(neighbours2.begin(), neighbours2.end(), vertex1), neighbours2.end());
+}
+
+// Get vertices
+vector<int> Graph::get_vertices()
+{
+    // Get list of all vertices in Graph/adjacency list
+    vector<int> vertices = vector<int>();
+
+    for (auto &[key, val] : adjacencyList)
+        vertices.push_back(key);
+
+    // return vertices
+    return vertices;
+}
+
+// Get Edges
+vector<pair<int, int>> Graph::get_edges()
+{
+    vector<pair<int, int>> edges;
+
+    for (auto &[vertex, neighbours] : adjacencyList)
+        for (auto &x : neighbours)
+            if (vertex < x)
+                edges.push_back(make_pair(vertex, x));
+
+    return edges;
+}
+
+// Get neighbours
+vector<int> Graph::get_neighbors(int vertex)
+{
+    // Return the neighbors
+    return adjacencyList[vertex];
+}
+
+// Is adjacent
+bool Graph::is_adjacent(int vertex1, int vertex2)
+{
+    auto &list = adjacencyList[vertex1];
+
+    return find(list.begin(), list.end(), vertex2) != list.end();
+}
+
+// Get vertex count
+int Graph::get_vertex_count()
+{
+    // get the count of vertices
+    return adjacencyList.size();
+}
+
+// Get edge count
+int Graph::get_edge_count()
+{
+    int count = 0;
+    for (auto &entries : adjacencyList)
+        count += entries.second.size();
+
+    return count / 2; // because in an undirected graph they are two for one
+}
+
 int main()
 {
     Graph graph;
@@ -83,6 +152,7 @@ int main()
     graph.add_vertex(4);
     graph.add_vertex(5);
     graph.add_vertex(6);
+
     graph.add_edge(1, 2);
     graph.add_edge(1, 3);
     graph.add_edge(2, 3);
@@ -90,47 +160,45 @@ int main()
     graph.add_edge(4, 5);
     graph.add_edge(5, 6);
 
-    std::cout << "Vertices: ";
+    cout << "Vertices: ";
     for (int vertex : graph.get_vertices())
-    {
-        std::cout << vertex << " ";
-    }
-    std::cout << std::endl;
+        cout << vertex << " ";
 
-    std::cout << "Edges: ";
+    cout << endl;
+
+    cout << "Edges: ";
     for (auto edge : graph.get_edges())
-    {
-        std::cout << "(" << edge.first << ", " << edge.second << ") ";
-    }
-    std::cout << std::endl;
+        cout << "(" << edge.first << ", " << edge.second << ") ";
 
-    std::cout << "Neighbors of vertex 1: ";
+    cout << endl;
+
+    cout << "Neighbors of vertex 1: ";
     for (int neighbor : graph.get_neighbors(1))
     {
-        std::cout << neighbor << " ";
+        cout << neighbor << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 
-    std::cout << "Is vertex 1 adjacent to vertex 2? " << std::boolalpha << graph.is_adjacent(1, 2) << std::endl;
+    cout << "Is vertex 1 adjacent to vertex 2? " << boolalpha << graph.is_adjacent(1, 2) << endl;
 
     graph.remove_edge(1, 2);
     graph.remove_vertex(3);
 
-    std::cout << "After removing some edges and vertices:" << std::endl;
+    cout << "After removing some edges and vertices:" << endl;
 
-    std::cout << "Vertices: ";
+    cout << "Vertices: ";
     for (int vertex : graph.get_vertices())
     {
-        std::cout << vertex << " ";
+        cout << vertex << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 
-    std::cout << "Edges: ";
+    cout << "Edges: ";
     for (auto edge : graph.get_edges())
     {
-        std::cout << "(" << edge.first << ", " << edge.second << ") ";
+        cout << "(" << edge.first << ", " << edge.second << ") ";
     }
-    std::cout << std::endl;
+    cout << endl;
 
     return 0;
 }
