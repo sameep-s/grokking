7854286dad1601e0e7938c7d0110a8f3e6546ef6
sameep <sameep.sharma98@gmail.com>
2024-11-03 18:40:08 +0530

    DecimalToBinary

--- 2_recursion/intro/easy/convert_decimal_2_binary.cpp
+++ 2_recursion/intro/easy/convert_decimal_2_binary.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 class Solution
 {
 public:
-    int decimalToBinary(int decimal)
+    string decimalToBinary(int decimal)
     {
+        if (decimal == 0)
+            return "";
 
-        return 0;
+        return decimalToBinary(decimal / 2) + to_string(decimal % 2);
     }
 };
 
 int main()
 {
     Solution s;
-    cout << s.decimalToBinary(20) << endl;
+    cout << s.decimalToBinary(10) << endl;
 
     return 0;
 }
