c80464b515b0cd32c70916a886776d6f8b54e2d0
sameep <sameep.sharma98@gmail.com>
2024-11-19 00:06:49 +0530

    Implement binary search to check for perfect squares in C++

---
+++ 2_recursion/intro/easy/6_perfect_sq.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isPerfectSqHelper(int num, int l, int r)
+    {
+        // Base case
+        if (l > r)
+            return false;
+
+        int mid = (l + r) / 2;
+        long long midSq = mid * mid;
+
+        // Recursive case
+        if (midSq == num)
+            return true;
+        else if (midSq > num)
+            return isPerfectSqHelper(num, l, mid - 1);
+        else
+            return isPerfectSqHelper(num, mid + 1, r);
+    }
+
+    // Can we use binary search for perfect square
+    bool isPerfectSquare(int num)
+    {
+        return isPerfectSqHelper(num, 0, num);
+    }
+};
+
+int main()
+{
+    Solution s;
+    bool isPerfectSq = s.isPerfectSquare(121);
+
+    printf("The perfect square is: %d \n", isPerfectSq);
+
+    return 0;
+}
