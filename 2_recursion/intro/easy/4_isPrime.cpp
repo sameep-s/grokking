fab4dfc1e5a226d5013bafe94ecfe35867bc446f
sameep <sameep.sharma98@gmail.com>
2024-11-22 01:44:32 +0530

    en: practive isPrime

--- 2_recursion/intro/easy/4_isPrime.cpp
+++ 2_recursion/intro/easy/4_isPrime.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <stdio.h>
 #include <cmath>
 
 using namespace std;
@@ -35,9 +35,46 @@ public:
     }
 };
 
+class Sol
+{
+public:
+    bool isPrime(int num)
+    {
+        // Base cases
+        if (num == 2)
+            return true;
+
+        if (num < 2)
+            return false;
+
+        //  Recursive case
+        return checkDivisor(num, 2);
+    }
+
+    bool checkDivisor(int num, int div)
+    {
+        // if divisor is greate than sqrt of num, then it is prime
+        if (div > sqrt(num))
+            return true;
+
+        // if num can be divided by divisor then not
+        if (num % div == 0)
+            return false;
+
+        return checkDivisor(num, div + 1);
+    }
+};
+
 int main()
 {
     Solution s;
+    Sol s2;
+
+    int isPrime = s.isPrime(10);
+    int isPrime2 = s2.isPrime(8);
+
+    printf("The prime is: %d \n", isPrime);
+    printf("The prime 2 is: %d \n", isPrime2);
 
     return 0;
 }
