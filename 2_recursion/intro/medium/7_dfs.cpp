da57664dbd0d76511a801bdc2fcd8f196391dbbf
sameep <sameep.sharma98@gmail.com>
2024-12-01 17:12:36 +0530

    en: remove node from linked list

--- 2_recursion/intro/medium/7_dfs.cpp
+++ 2_recursion/intro/medium/7_dfs.cpp
@@ -58,7 +58,6 @@ int main()
     Solution graph;
     vector<vector<int>> args = {{1, 2}, {1, 3}, {1, 4}, {2, 5}, {4, 5}};
 
-    // vector<vector<int>> args = {{1, 2}, {1, 5}, {2, 4}, {4, 5}, {3, 5}};
     vector<int> result = graph.DFS(args, 6, 1);
 
     printf("DFS traversal: ");
---
+++ 2_recursion/intro/medium/8_rm_node_linkedList.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+
+    ListNode(int val) : val(val), next(nullptr) {}
+};
+
+class Solution
+{
+public:
+    ListNode *removeNodes(ListNode *head, int target)
+    {
+        //   Base case
+        if (!head)
+            return nullptr;
+
+        //   Recursive  case
+        if (head->val != target)
+            head->next = removeNodes(head->next, target);
+        else
+            head = head->next;
+
+        return head;
+    }
+
+    void printLinkedList(ListNode *start)
+    {
+        if (!start)
+        {
+            printf("\n");
+            return;
+        }
+
+        printf("%d ", start->val);
+        printLinkedList(start->next);
+    }
+};
+
+int main()
+{
+    Solution sol;
+
+    // Example 1
+    ListNode *head1 = new ListNode(1);
+    head1->next = new ListNode(2);
+    head1->next->next = new ListNode(3);
+    head1->next->next->next = new ListNode(4);
+    head1->next->next->next->next = new ListNode(2);
+
+    std::cout << "Original Linked List 1: ";
+    sol.printLinkedList(head1);
+
+    // Remove nodes with value 2 from linked list
+    ListNode *modifiedHead1 = sol.removeNodes(head1, 2);
+    std::cout << "Modified Linked List 1: ";
+    sol.printLinkedList(modifiedHead1);
+
+    // Example 2
+    ListNode *head2 = new ListNode(2);
+    head2->next = new ListNode(2);
+    head2->next->next = new ListNode(2);
+    head2->next->next->next = new ListNode(2);
+    head2->next->next->next->next = new ListNode(2);
+
+    std::cout << "Original Linked List 2: ";
+    sol.printLinkedList(head2);
+
+    // Remove nodes with value 2 from linked list
+    ListNode *modifiedHead2 = sol.removeNodes(head2, 2);
+    std::cout << "Modified Linked List 2: ";
+    sol.printLinkedList(modifiedHead2);
+
+    // Example 3
+    ListNode *head3 = new ListNode(1);
+    head3->next = new ListNode(3);
+    head3->next->next = new ListNode(5);
+    head3->next->next->next = new ListNode(7);
+
+    std::cout << "Original Linked List 3: ";
+    sol.printLinkedList(head3);
+
+    // Remove nodes with value 2 from linked list
+    ListNode *modifiedHead3 = sol.removeNodes(head3, 2);
+    std::cout << "Modified Linked List 3: ";
+    sol.printLinkedList(modifiedHead3);
+
+    return 0;
+}
