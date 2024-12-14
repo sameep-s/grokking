2c9941c1b5c4ecda07335c545bd9456c3b66e5db
sameep <sameep.sharma98@gmail.com>
2024-12-14 17:51:08 +0530

    En: fix fibonacci base case & improve handling of examples in main

--- 2_recursion/intro/hard/1_fib.cpp
+++ 2_recursion/intro/hard/1_fib.cpp
@@ -7,18 +7,20 @@ using namespace std;
 class Solution
 {
 public:
+    // Time complexity - O(N)
+    // Space complexity - O(N)
     vector<int> fibonacci(int n)
     {
         if (n <= 0)
-            return vector<int>();
+            return vector<int>(1, 0);
 
-        vector<int> res(n);
+        vector<int> res(n + 1);
         res[0] = 0;
+        res[1] = 1;
 
         if (n == 1)
             return res;
 
-        res[1] = 1;
         fibHelper(n, 2, res);
 
         return res;
@@ -26,13 +28,11 @@ public:
 
     void fibHelper(int n, int start, vector<int> &res)
     {
-        if (n == start)
+        if (start > n)
             return;
 
         int val = res[start - 2] + res[start - 1];
-
         res[start] = val;
-
         fibHelper(n, start + 1, res);
     }
 };
@@ -41,7 +41,7 @@ int main()
 {
     Solution sol;
     // Example inputs
-    int examples[] = {5, 8, 12};
+    int examples[] = {2, 5, 8, 12};
     int numExamples = sizeof(examples) / sizeof(examples[0]);
 
     for (int i = 0; i < numExamples; i++)
