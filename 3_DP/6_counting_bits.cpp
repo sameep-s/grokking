b94c0e46a6f6c4f1d7a1e298dcb15938e740279b
sameep <sameep.sharma98@gmail.com>
2025-01-21 01:25:50 +0530

    counting bits

---
+++ 3_DP/6_counting_bits.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  vector<int> countBits(int n)
+  {
+    vector<int> bits(n + 1);
+
+    for (int i = 1; i <= n; i++)
+    {
+      int val = i / 2;
+
+      if (i % 2 == 0) // even
+        bits[i] = bits[val];
+      else // odd
+        bits[i] = bits[val] + 1;
+    }
+
+    return bits;
+  }
+};
+
+int main()
+{
+  Solution sol;
+  vector<int> bitsArr = sol.countBits(5);
+
+  return 0;
+}
